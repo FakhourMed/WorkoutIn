2ef29ed6d6d2193c66dbb1a4f652f668326da697
Fakhour Mohammed <mohammed.fakhour@babel.es>
2021-12-31 11:46:24 +0100

    Initial commit

---
+++ app/src/main/java/com/fakhour/workoutin/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.fakhour.workoutin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rbddevs.splashy.Splashy
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSplashy()
+
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+
+    fun setSplashy(){
+        Splashy(this) 		 // For JAVA : new Splashy(this)
+            .setLogo(R.drawable.ic_logo)
+            .setTitle("WorkoutIN")
+            .setTitleColor("#FFFFFF")
+            .setSubTitle("Home workout made easy")
+            .setProgressColor(R.color.white)
+            .setBackgroundResource(R.color.purple_700)
+            .setFullScreen(true)
+            .setDuration(2000)
+            .show()
+    }
+}
