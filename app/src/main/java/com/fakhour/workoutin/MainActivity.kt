705beaca841360f973ae4ef903e10c6b14dc55e5
Fakhour Mohammed <mohammed.fakhour@babel.es>
2022-01-02 11:32:40 +0100

    - saved instance state used to display splashScreen on time per App launch, not after each destroy/create

--- app/src/main/java/com/fakhour/workoutin/MainActivity.kt
+++ app/src/main/java/com/fakhour/workoutin/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.fakhour.workoutin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.fakhour.workoutin.workout.workout_sections.WorkoutSectionViewModel
+import androidx.appcompat.app.AppCompatActivity
 import com.rbddevs.splashy.Splashy
 
+const val IS_SPLASH_SCREEN_DISPLAYED = "package com.fakhour.workoutin.IS_SPLASH_SCREEN_DISPLAYED"
+var isSplashScreenDisplayed = false
+
 class MainActivity : AppCompatActivity() {
-    private val mainActivityViewModel: MainActivityViewModel by lazy {
-        ViewModelProvider(this).get(MainActivityViewModel::class.java)
-    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState?.getBoolean(IS_SPLASH_SCREEN_DISPLAYED) == false || savedInstanceState?.getBoolean(IS_SPLASH_SCREEN_DISPLAYED) == null) {
+            setSplashy()
+        }
+
         setContentView(R.layout.activity_main)
 
-        mainActivityViewModel.setSplashy(this)
+
     }
 
+    fun setSplashy() {
+        Splashy(this)
+            .setLogo(R.drawable.ic_logo)
+            .setTitle("WorkoutIN")
+            .setTitleColor("#FFFFFF")
+            .setSubTitle("Home workout made easy")
+            .setProgressColor(R.color.white)
+            .setBackgroundResource(R.mipmap.background)
+            .setFullScreen(true)
+            .setDuration(2000)
+            .show()
 
+        isSplashScreenDisplayed = true
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putBoolean(IS_SPLASH_SCREEN_DISPLAYED, isSplashScreenDisplayed)
+
+    }
 }
